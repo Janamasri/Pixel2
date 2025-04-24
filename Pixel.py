08e2e5797db7b2177a52bbba3074844add439d73
Janamasri <115082682+Janamasri@users.noreply.github.com>
2025-04-24 18:58:07 +0300

    Pixel.py

--- Pixel.py
+++ Pixel.py
@@ -9,7 +9,7 @@ from xgboost import XGBRegressor
 # Prophet setup with cmdstanpy (no need for pystan)
 from prophet import Prophet
 import cmdstanpy
-cmdstanpy.install_cmdstan("Pixel.py")
+cmdstanpy.install_cmdstan()
 
 # ================================
 # Pixel Digital - Growth Dashboard (Extended Version)
